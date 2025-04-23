2e6942ada28984a25b33e8c6e7ee800b4a60c120
farzadmsv14 <farzadmsv14@gmail.com>
2025-04-23 09:57:49 +0330

    menu-v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'headerTailwind';
+  isMobile = false;
+  isDrawerOpen = false;
+  isUserMenuOpen = false;
+
+  user = {
+    name: 'فرزاد موسوی',
+    avatar: 'https://i.pravatar.cc/100?img=12',
+  };
+
+  ngOnInit() {
+    this.checkScreenSize();
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.checkScreenSize();
+  }
+
+  private checkScreenSize() {
+    this.isMobile = window.innerWidth < 950;
+    if (!this.isMobile) {
+      this.isDrawerOpen = false;
+    }
+  }
+
+  toggleDrawer() {
+    this.isDrawerOpen = !this.isDrawerOpen;
+  }
+
+  toggleUserMenu() {
+    this.isUserMenuOpen = !this.isUserMenuOpen;
+  }
+
+  logout() {
+    // عملیات خروج از حساب
+    alert('خروج انجام شد!');
+  }
 }
